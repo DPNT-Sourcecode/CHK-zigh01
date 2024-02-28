078bb030206c102f66a6a286c61365cea2c1682c
sbx_user1051 <sbx_user1051@169.254.10.245>
2024-02-28 21:02:30 +0000

    Wed Feb 28 21:02:30 UTC 2024

--- src/test/java/befaster/solutions/TST/CheckoutTest.java
+++ src/test/java/befaster/solutions/TST/CheckoutTest.java
@@ -21,6 +21,35 @@ public class CheckoutTest {
     int price = solution.checkout("AAABCD");
     assertThat(price, equalTo(195));
   }
+
+  @Test
+  public void testCheckoutWithOfferAndMore() {
+    CheckoutSolution solution = new CheckoutSolution();
+    int price = solution.checkout("AAAAABCD");
+    assertThat(price, equalTo(295));
+  }
+
+
+  @Test
+  public void testCheckoutWithSameOfferTwice() {
+    CheckoutSolution solution = new CheckoutSolution();
+    int price = solution.checkout("AAAAAABCD");
+    assertThat(price, equalTo(325));
+  }
+  @Test
+  public void testCheckoutWithTwoDifferent() {
+    CheckoutSolution solution = new CheckoutSolution();
+    int price = solution.checkout("AAAAAABBCD");
+    assertThat(price, equalTo(340));
+  }
+
+  @Test
+  public void testCheckoutWithInvalid() {
+    CheckoutSolution solution = new CheckoutSolution();
+    int price = solution.checkout("aaabcd");
+    assertThat(price, equalTo(-1));
+  }
 }
+
 
 
