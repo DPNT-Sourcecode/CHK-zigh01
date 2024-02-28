f05d044a2a41cdaec27b8b11a73672f83b523f5c
sbx_user1051 <sbx_user1051@169.254.10.245>
2024-02-28 21:08:31 +0000

    Wed Feb 28 21:08:31 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,26 +5,37 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
 
 public class CheckoutSolution {
-    class Offer implements Comparable {
+    static class Offer implements Comparable<Offer> {
+        enum Type {
+            DISCOUNT,
+            GET_ONE_FREE
+        }
         private int number;
         private int price;
+        private Character freeItem;
 
-        public Offer(int number, int price) {
+        private Type type;
+
+        public Offer(int number, int price, Character freeItem, Type type) {
             this.number = number;
             this.price = price;
+            this.freeItem = freeItem;
+            this.type = type;
         }
 
         @Override
-        public int comp
+        public int compareTo(Offer o) {
+            return o.number - this.number;
+        }
     }
 
     private Map<Character, Integer> priceMap;
-    private Map<Character, Offer> offers;
-
-
+    private Map<Character, Queue<Offer>> offers;
 
     public CheckoutSolution() {
         priceMap = Map.of(
@@ -34,7 +45,7 @@ public class CheckoutSolution {
             'D',15
         );
         offers = Map.of(
-            'A', new Offer(3, priceMap.get('A') * 3 - 130),
+            'A', new Offer(3, new PriorityQueue<>()),
             'B', new Offer(2, priceMap.get('B') * 2 - 45)
         );
     }
@@ -72,6 +83,7 @@ public class CheckoutSolution {
         return price;
     }
 }
+
 
 
 
