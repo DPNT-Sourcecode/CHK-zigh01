7e813acde0891b728e5890f3af5869a9ab2143ec
sbx_user1051 <sbx_user1051@169.254.10.245>
2024-02-28 22:39:08 +0000

    Wed Feb 28 22:39:08 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -14,191 +14,207 @@ import java.util.Queue;
 import java.util.Set;
 
 public class CheckoutSolution {
-    static class Offer implements Comparable<Offer> {
-        enum Type {
-            DISCOUNT,
-            GET_ONE_FREE,
-            BUNDLED_ITEMS
-        }
-        private int number;
-        private int price;
-        private Set<Character> discountedItems;
 
-        private Type type;
+  static class Offer implements Comparable<Offer> {
 
-        public Offer(Type type, int number, int price, Set<Character> discountedItems) {
-            this.number = number;
-            this.price = price;
-            this.discountedItems = discountedItems;
-            this.type = type;
-        }
-
-        @Override
-        public int compareTo(Offer o) {
-            return o.number - this.number;
-        }
+    enum Type {
+      DISCOUNT,
+      GET_ONE_FREE,
+      BUNDLED_ITEMS
     }
 
-    private Map<Character, Integer> priceMap;
-    private Map<Character, List<Offer>> offersDetails;
+    private int number;
+    private int price;
+    private Set<Character> discountedItems;
 
-    private Map<Type, List<Character>> offers;
+    private Type type;
 
-    public CheckoutSolution() {
-        priceMap = new HashMap<>();
-        priceMap.put('A', 50);
-        priceMap.put('B', 30);
-        priceMap.put('C', 20);
-        priceMap.put('D', 15);
-        priceMap.put('E', 40);
-        priceMap.put('F', 10);
-        priceMap.put('G', 20);
-        priceMap.put('H', 10);
-        priceMap.put('I', 35);
-        priceMap.put('J', 60);
-        priceMap.put('K', 70);
-        priceMap.put('L', 90);
-        priceMap.put('M', 15);
-        priceMap.put('N', 40);
-        priceMap.put('O', 10);
-        priceMap.put('P', 50);
-        priceMap.put('Q', 30);
-        priceMap.put('R', 50);
-        priceMap.put('S', 20);
-        priceMap.put('T', 20);
-        priceMap.put('U', 40);
-        priceMap.put('V', 50);
-        priceMap.put('W', 20);
-        priceMap.put('X', 17);
-        priceMap.put('Y', 20);
-        priceMap.put('Z', 21);
+    public Offer(Type type, int number, int price, Set<Character> discountedItems) {
+      this.number = number;
+      this.price = price;
+      this.discountedItems = discountedItems;
+      this.type = type;
+    }
 
-        offersDetails = new HashMap<>();
-        offersDetails.put('A', new ArrayList<>());
-        offersDetails.put('B', new ArrayList<>());
-        offersDetails.put('E', new ArrayList<>());
-        offersDetails.put('F', new ArrayList<>());
-        offersDetails.put('H', new ArrayList<>());
-        offersDetails.put('K', new ArrayList<>());
-        offersDetails.put('N', new ArrayList<>());
-        offersDetails.put('P', new ArrayList<>());
-        offersDetails.put('Q', new ArrayList<>());
-        offersDetails.put('R', new ArrayList<>());
-        offersDetails.put('U', new ArrayList<>());
-        offersDetails.put('V', new ArrayList<>());
+    @Override
+    public int compareTo(Offer o) {
+      return o.number - this.number;
+    }
+  }
 
-        offers = Map.of(
-            Type.GET_ONE_FREE, List.of('E', 'F', 'N', 'R', 'U'),
-            Type.DISCOUNT, List.of('A', 'B', 'H', 'K', 'P', 'Q', 'V'),
-            Type.BUNDLED_ITEMS, List.of('S', 'T', 'X', 'Y', 'Z')
+  private Map<Character, Integer> priceMap;
+  private Map<Character, List<Offer>> offersDetails;
+
+  private Map<Type, List<Character>> offers;
+
+  public CheckoutSolution() {
+    priceMap = new HashMap<>();
+    priceMap.put('A', 50);
+    priceMap.put('B', 30);
+    priceMap.put('C', 20);
+    priceMap.put('D', 15);
+    priceMap.put('E', 40);
+    priceMap.put('F', 10);
+    priceMap.put('G', 20);
+    priceMap.put('H', 10);
+    priceMap.put('I', 35);
+    priceMap.put('J', 60);
+    priceMap.put('K', 70);
+    priceMap.put('L', 90);
+    priceMap.put('M', 15);
+    priceMap.put('N', 40);
+    priceMap.put('O', 10);
+    priceMap.put('P', 50);
+    priceMap.put('Q', 30);
+    priceMap.put('R', 50);
+    priceMap.put('S', 20);
+    priceMap.put('T', 20);
+    priceMap.put('U', 40);
+    priceMap.put('V', 50);
+    priceMap.put('W', 20);
+    priceMap.put('X', 17);
+    priceMap.put('Y', 20);
+    priceMap.put('Z', 21);
+
+    offersDetails = new HashMap<>();
+    offersDetails.put('A', new ArrayList<>());
+    offersDetails.put('B', new ArrayList<>());
+    offersDetails.put('E', new ArrayList<>());
+    offersDetails.put('F', new ArrayList<>());
+    offersDetails.put('H', new ArrayList<>());
+    offersDetails.put('K', new ArrayList<>());
+    offersDetails.put('N', new ArrayList<>());
+    offersDetails.put('P', new ArrayList<>());
+    offersDetails.put('Q', new ArrayList<>());
+    offersDetails.put('R', new ArrayList<>());
+    offersDetails.put('U', new ArrayList<>());
+    offersDetails.put('V', new ArrayList<>());
+    offersDetails.put('S', new ArrayList<>());
+    offersDetails.put('T', new ArrayList<>());
+    offersDetails.put('X', new ArrayList<>());
+    offersDetails.put('Y', new ArrayList<>());
+    offersDetails.put('Z', new ArrayList<>());
+
+    offers = Map.of(
+        Type.GET_ONE_FREE, List.of('E', 'F', 'N', 'R', 'U'),
+        Type.DISCOUNT, List.of('A', 'B', 'H', 'K', 'P', 'Q', 'V'),
+        Type.BUNDLED_ITEMS, List.of('S', 'T', 'X', 'Y', 'Z')
+    );
+
+    offersDetails.get('A').add(new Offer(Type.DISCOUNT, 5, priceMap.get('A') * 5 - 200, null));
+    offersDetails.get('A').add(new Offer(Type.DISCOUNT, 3, priceMap.get('A') * 3 - 130, null));
+    offersDetails.get('B').add(new Offer(Type.DISCOUNT, 2, priceMap.get('B') * 2 - 45, null));
+    offersDetails.get('H').add(new Offer(Type.DISCOUNT, 10, priceMap.get('H') * 10 - 80, null));
+    offersDetails.get('H').add(new Offer(Type.DISCOUNT, 5, priceMap.get('H') * 5 - 45, null));
+    offersDetails.get('K').add(new Offer(Type.DISCOUNT, 2, priceMap.get('K') * 2 - 120, null));
+    offersDetails.get('P').add(new Offer(Type.DISCOUNT, 5, priceMap.get('P') * 5 - 200, null));
+    offersDetails.get('Q').add(new Offer(Type.DISCOUNT, 3, priceMap.get('Q') * 3 - 80, null));
+    offersDetails.get('V').add(new Offer(Type.DISCOUNT, 3, priceMap.get('V') * 3 - 130, null));
+    offersDetails.get('V').add(new Offer(Type.DISCOUNT, 2, priceMap.get('V') * 2 - 90, null));
+
+    offersDetails.get('E').add(new Offer(Type.GET_ONE_FREE, 2, priceMap.get('B'), Set.of('B')));
+    offersDetails.get('F').add(new Offer(Type.GET_ONE_FREE, 2, priceMap.get('F'), Set.of('F')));
+    offersDetails.get('N').add(new Offer(Type.GET_ONE_FREE, 3, priceMap.get('M'), Set.of('M')));
+    offersDetails.get('R').add(new Offer(Type.GET_ONE_FREE, 3, priceMap.get('Q'), Set.of('Q')));
+    offersDetails.get('U').add(new Offer(Type.GET_ONE_FREE, 3, priceMap.get('U'), Set.of('U')));
+
+    offersDetails.get('S')
+        .add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
+    offersDetails.get('T')
+        .add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
+    offersDetails.get('X')
+        .add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
+    offersDetails.get('Y')
+        .add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
+    offersDetails.get('Z')
+        .add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
+  }
+
+  public Integer checkout(String skus) {
+    Map<Character, Integer> basket = new HashMap<>();
+    Set<Character> offerableSkusInBasket = new HashSet<>();
+    int price = 0;
+
+    for (int i = 0; i < skus.length(); i++) {
+      char current = skus.charAt(i);
+      if (!priceMap.containsKey(current)) {
+        return -1;
+      }
+
+      if (offersDetails.containsKey(current)) {
+        offerableSkusInBasket.add(current);
+      }
+
+      basket.put(current, basket.getOrDefault(current, 0) + 1);
+
+      price += priceMap.get(current);
+    }
+
+    List<Character> getOneFreeOffers = offers.getOrDefault(Type.GET_ONE_FREE, new ArrayList<>());
+
+    for (Character offerSku : getOneFreeOffers) {
+      if (offerableSkusInBasket.contains(offerSku)) {
+        price -= getDeductionsFromOffer(
+            offerableSkusInBasket,
+            offerSku,
+            basket
         );
-
-        offersDetails.get('A').add(new Offer(Type.DISCOUNT, 5,   priceMap.get('A') * 5 - 200, null));
-        offersDetails.get('A').add(new Offer(Type.DISCOUNT, 3, priceMap.get('A') * 3 - 130, null));
-        offersDetails.get('B').add(new Offer(Type.DISCOUNT, 2, priceMap.get('B') * 2 - 45, null));
-        offersDetails.get('H').add(new Offer(Type.DISCOUNT, 10, priceMap.get('H') * 10 - 80, null));
-        offersDetails.get('H').add(new Offer(Type.DISCOUNT, 5, priceMap.get('H') * 5 - 45, null));
-        offersDetails.get('K').add(new Offer(Type.DISCOUNT, 2, priceMap.get('K') * 2 - 120, null));
-        offersDetails.get('P').add(new Offer(Type.DISCOUNT, 5, priceMap.get('P') * 5 - 200, null));
-        offersDetails.get('Q').add(new Offer(Type.DISCOUNT, 3, priceMap.get('Q') * 3 - 80, null));
-        offersDetails.get('V').add(new Offer(Type.DISCOUNT, 3, priceMap.get('V') * 3 - 130, null));
-        offersDetails.get('V').add(new Offer(Type.DISCOUNT, 2, priceMap.get('V') * 2 - 90, null));
-
-
-        offersDetails.get('E').add(new Offer(Type.GET_ONE_FREE, 2, priceMap.get('B'), Set.of('B')));
-        offersDetails.get('F').add(new Offer(Type.GET_ONE_FREE, 2, priceMap.get('F'), Set.of('F')));
-        offersDetails.get('N').add(new Offer(Type.GET_ONE_FREE, 3, priceMap.get('M'), Set.of('M')));
-        offersDetails.get('R').add(new Offer(Type.GET_ONE_FREE, 3, priceMap.get('Q'), Set.of('Q')));
-        offersDetails.get('U').add(new Offer(Type.GET_ONE_FREE, 3, priceMap.get('U'), Set.of('U')));
-
-        offersDetails.get('S').add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
-        offersDetails.get('T').add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
-        offersDetails.get('X').add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
-        offersDetails.get('Y').add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
-        offersDetails.get('Z').add(new Offer(Type.BUNDLED_ITEMS, 3, 45, Set.of('S', 'T', 'X', 'Y', 'Z')));
+      }
     }
 
-    public Integer checkout(String skus) {
-        Map<Character, Integer> basket = new HashMap<>();
-        Set<Character> offerableSkusInBasket = new HashSet<>();
-        int price = 0;
-
-        for (int i = 0; i < skus.length(); i++) {
-            char current = skus.charAt(i);
-            if (!priceMap.containsKey(current)) {
-                return -1;
-            }
-
-            if (offersDetails.containsKey(current)) {
-                offerableSkusInBasket.add(current);
-            }
-
-            basket.put(current, basket.getOrDefault(current, 0) + 1);
-
-            price += priceMap.get(current);
-        }
-
-        List<Character> getOneFreeOffers = offers.getOrDefault(Type.GET_ONE_FREE, new ArrayList<>());
-
-        for (Character offerSku: getOneFreeOffers) {
-            if (offerableSkusInBasket.contains(offerSku)) {
-               price -= getDeductionsFromOffer(
-                   offerableSkusInBasket,
-                   offerSku,
-                   basket
-               );
-            }
-        }
-
-        List<Character> discountSkus = offers.getOrDefault(Type.DISCOUNT, new ArrayList<>());
-        for (Character offerSku: discountSkus) {
-            if (offerableSkusInBasket.contains(offerSku)) {
-                price -= getDeductionsFromOffer(
-                    offerableSkusInBasket,
-                    offerSku,
-                    basket
-                );
-            }
-        }
-
-        return price;
+    List<Character> discountSkus = offers.getOrDefault(Type.DISCOUNT, new ArrayList<>());
+    for (Character offerSku : discountSkus) {
+      if (offerableSkusInBasket.contains(offerSku)) {
+        price -= getDeductionsFromOffer(
+            offerableSkusInBasket,
+            offerSku,
+            basket
+        );
+      }
     }
 
-    private int getDeductionsFromOffer(
-        Set<Character> offerableSkusInBasket,
-        Character offerSku,
-        Map<Character, Integer> basket
-    ) {
-        int deductions = 0;
-        while (offerableSkusInBasket.contains(offerSku)) {
-            char sku = offerSku;
-            int number = basket.getOrDefault(sku,0);
-            List<Offer> offerList = offersDetails.get(sku);
-            boolean matchingOffer = false;
-            for (Offer offer: offerList) {
-                while (offer.number <= number) {
-                    matchingOffer = true;
-                    number = number - offer.number;
-                    basket.put(sku, number);
-                    if (offer.type == Type.DISCOUNT) {
-                        deductions += offer.price;
-                    } else {
-                        int numberOfRemovableItems = basket.getOrDefault(offer.discountedItems.stream().findFirst().get(), 0);
-                        if (numberOfRemovableItems != 0) {
-                            basket.put(offer.discountedItems, numberOfRemovableItems - 1);
-                            if (numberOfRemovableItems - 1 == 0) {
-                                offerableSkusInBasket.remove(offer.discountedItems);
-                            }
-                            deductions += offer.price;
-                        }
-                    }
-                }
+    return price;
+  }
+
+  private int getDeductionsFromOffer(
+      Set<Character> offerableSkusInBasket,
+      Character offerSku,
+      Map<Character, Integer> basket
+  ) {
+    int deductions = 0;
+    while (offerableSkusInBasket.contains(offerSku)) {
+      char sku = offerSku;
+      int number = basket.getOrDefault(sku, 0);
+      List<Offer> offerList = offersDetails.get(sku);
+      boolean matchingOffer = false;
+      for (Offer offer : offerList) {
+        while (offer.number <= number) {
+          matchingOffer = true;
+          number = number - offer.number;
+          basket.put(sku, number);
+          if (offer.type == Type.DISCOUNT) {
+            deductions += offer.price;
+          } else if (offer.type == Type.GET_ONE_FREE) {
+            char removable = offer.discountedItems.stream().findFirst().orElse('~');
+            int numberOfRemovableItems = basket.getOrDefault(removable, 0);
+            if (numberOfRemovableItems != 0) {
+              basket.put(removable, numberOfRemovableItems - 1);
+              if (numberOfRemovableItems - 1 == 0) {
+                offerableSkusInBasket.remove(removable);
+              }
+              deductions += offer.price;
             }
-            if (!matchingOffer) {
-                offerableSkusInBasket.remove(sku);
-            }
+          } else {
+
+          }
         }
-        return deductions;
+      }
+      if (!matchingOffer) {
+        offerableSkusInBasket.remove(sku);
+      }
     }
+    return deductions;
+  }
 }
+
 
